4a8a8e61e262a9afeb83bdba88f31d3b7b7aaa15
Ravi <ravi2krishna@gmail.com>
2023-01-06 10:39:32 +0530

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,5 +2,6 @@
 sudo yum -y install epel-release
 sudo yum -y install ansible
 sudo yum -y install git
-git clone <ansible-repo>
+git clone https://github.com/ravi2krishna/tf-multiple-mdp.git /home/centos
+cd tf-multiple-mdp
 ansible-playbook setup_ecomm.yml -i aws.ini
